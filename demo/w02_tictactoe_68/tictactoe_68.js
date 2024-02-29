43b244849ea927e127b7e692a26336094511f9d3
212410368 <212410368@o365.tku.edu.tw>
2024-02-29 19:56:52 +0800

    W02-P2: implement reset button

---
+++ demo/w02_tictactoe_68/tictactoe_68.js
@@ -0,0 +1,24 @@
+const o = 'o';
+const x = 'x';
+let turn = 0;
+let done = false;
+
+const container = document.querySelector('#container');
+const allLi = document.querySelectorAll('#board li');
+const resetBtn = document.querySelector('#reset');
+
+console.log('allLi', allLi);
+console.log('container', container);
+console.log('resetBtn', resetBtn);
+
+const reset = () => {
+  allLi.forEach((item) => {
+    item.classList = '';
+    item.textContent = '+';
+  });
+  container.style.backgroundColor = '#666';
+  turn = 0;
+  done = false;
+};
+
+resetBtn.addEventListener('click', reset);
