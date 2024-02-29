0e2ef5eb26d25b42889fb13b2a9d5af2f43961fc
212410368 <212410368@o365.tku.edu.tw>
2024-02-29 20:58:56 +0800

    W02-P3: implment checkWin(player), and winMessage(player)

--- demo/w02_tictactoe_68/tictactoe_68.js
+++ demo/w02_tictactoe_68/tictactoe_68.js
@@ -21,4 +21,62 @@ const reset = () => {
   done = false;
 };
 
+const checkWin = (player) => {
+  let p = [];
+  allLi.forEach((item) => {
+    p.push(item.classList.contains(player));
+  });
+  console.log('p', p);
+  const [p1, p2, p3, p4, p5, p6, p7, p8, p9] = p;
+  if (
+    (p1 && p2 && p3) ||
+    (p4 && p5 && p6) ||
+    (p7 && p8 && p9) ||
+    (p1 && p4 && p7) ||
+    (p2 && p5 && p8) ||
+    (p3 && p6 && p9) ||
+    (p1 && p5 && p9) ||
+    (p3 && p5 && p7)
+  )
+    return true;
+  else return false;
+};
+
+const winMessage = (player) => {
+  if (player === 'o') {
+    container.style.backgroundColor = 'rgba(144, 238, 144, 0.5)';
+  } else {
+    container.style.backgroundColor = 'rgba(240, 118, 128, 0.726)';
+  }
+  alert(`player ${player} wins`);
+};
+
+allLi.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (item.classList.contains('disabled')) {
+      alert('already filled');
+    } else {
+      if (turn % 2 === 0) {
+        item.textContent = 'o';
+        item.classList.add('o', 'disabled');
+        if (checkWin('o')) {
+          winMessage('o');
+        }
+      } else if (turn % 2 === 1) {
+        item.textContent = 'x';
+        item.classList.add('x', 'disabled');
+        if (checkWin('x')) {
+          winMessage('x');
+        }
+      }
+
+      if (!done && turn < 8) {
+        turn++;
+      } else if (!done && turn >= 8) {
+        alert('tie');
+      }
+    }
+  });
+});
+
 resetBtn.addEventListener('click', reset);
